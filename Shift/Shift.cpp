fe9ec14023d55c88ec5a4c52a064370158119e81
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2022-03-22 21:21:43 +0300

    Add project files.

---
+++ Shift/Shift.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n] = { 1,2,3,4,5,6,7,8,9,10 };
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+	int namber_of_shifts;
+	cout << "¬ведите количество сдвигов:"; cin >> namber_of_shifts;
+	for (int i = 0; i < namber_of_shifts; i++)
+	{
+		int buffer = arr[0];
+		for (int i = 0; i < n; i++)
+		{
+			arr[i] = arr[i + 1];
+		}
+		arr[n - 1] = buffer;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+	
+
+}
---
+++ Solution'Arrays'/Arrays.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5; // количество элементов массива
+	int arr[n] = {};
+	int sum = 0;
+	int min, max;
+	min = max = arr[0];
+	double sr;
+	//arr[2] = 123;
+	cout << "Введите элементы массива (" << n << " шт):";
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	//вывод масива на экран
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+	//вывод массива на экран в обратном порядке:
+	for (int i = n - 1; i >= 0; i--)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > 0)
+		{
+			sum += arr[i];
+		}
+	}
+	cout << ("sum=%d\n", sum); cout << endl;
+	cout << "Среднеее:" << (double)sum / n;
+	cout << endl;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+		if (arr[i] > max)max = arr[i];
+	}
+	cout << "Минимальное значение массива:" << min << endl;
+	cout << "Максимальное значение массива:" << max << endl;
+	
+}
