dd51570daa365707306e97f5512b1ad7fe2e5e8c
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2022-03-31 23:28:27 +0300

    UniqueRand

--- UniqueRand/UniqueRand.cpp
+++ UniqueRand/UniqueRand.cpp
@@ -1,27 +1,53 @@
 #include<iostream>
 using namespace std;
 
+//#define TASK
+#define TASK_1
+
 #define tab "\t"
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	const int n = 10;
+#ifdef TASK
 	int arr[n] = {};
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % n;
-	for (int j = 1; j > n; j++)
-	{
-		if (arr[i] == arr[i - j])
+		for (int j = 1; j > n; j++)
 		{
-			if((i-j)>=0){}
+			if (arr[i] == arr[i - j])
+			{
+				if ((i - j) >= 0) {}
+			}
 		}
-	}
 	}
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << tab;
 	}
 	cout << endl;
+#endif // TASK
+#ifdef TASK_1
+	int arr[n] = {};
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % n;
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[i] == arr[j]) 
+			{
+				i--;
+				break;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+#endif // TASK_1
+
 }
